f3f723d33592ada768ff3f5f80645e43f7add20c
chaewon <cwlee4633@naver.com>
2024-02-07 18:11:30 +0900

    테이블 맵핑하기

---
+++ src/main/java/com/example/jpaprac/Member.java
@@ -0,0 +1,31 @@
+package com.example.jpaprac;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+
+@Entity
+public class Member {
+    @Id
+    private Long id;
+
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    public Member(){
+    }
+}
---
+++ src/main/java/com/example/jpaprac/RoleType.java
@@ -0,0 +1,5 @@
+package com.example.jpaprac;
+
+public enum RoleType {
+    USER, ADMIN
+}
